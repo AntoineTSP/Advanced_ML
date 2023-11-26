3c86fbf1ca9dc7b10945e4c65f0c17c4e0e8a0c5
tamadei <tristan.amadei@ensae.fr>
2023-11-26 15:38:31 +0000

    check convergence added

--- Embedding_imdb/TSNE_code/TSNE_utils.py
+++ Embedding_imdb/TSNE_code/TSNE_utils.py
@@ -17,9 +17,17 @@ def _compute_gradient(Y, P, Q, t_student_q_distances, n_components):
 
     return gradient
 
+def _check_convergence(gradient, min_grad_norm):
+    grad_norm = np.linalg.norm(gradient)
+    return abs(grad_norm) < min_grad_norm
+
+def _momentum(t):
+    return 0.5 if t < 250 else 0.8
+
 
 class TSNE():
-    def __init__(self, n_components, perplexity, early_exaggeration=4, iterations_with_early_exaggeration=50, learning_rate=None, metric="euclidean", n_iter=1000, n_steps_probas=100):
+    def __init__(self, n_components, perplexity, early_exaggeration=4, iterations_with_early_exaggeration=50, learning_rate=None, metric="euclidean", 
+                 n_iter=1000, n_steps_probas=100, min_grad_norm=1e-5, interval_convergence_check=50, momentum_rule=_momentum):
         self.n_components = n_components
         self.perplexity = perplexity
         self.early_exaggeration = early_exaggeration
@@ -28,6 +36,9 @@ class TSNE():
         self.metric = metric
         self.n_iter = max(250, n_iter)
         self.n_steps_probas = n_steps_probas
+        self.min_grad_norm = min_grad_norm
+        self.interval_convergence_check = interval_convergence_check
+        self.momentum_rule = momentum_rule
 
     def fit_transform(self, X, verbose=0):
         #squared_distances = pairwise_distances(X, metric=self.metric, squared=True)
@@ -44,7 +55,7 @@ class TSNE():
         if self.learning_rate is None:
             self.learning_rate = max(X.shape[0] / self.early_exaggeration / 4, 50)
 
-        range_ = tqdm(range(self.n_iter)) if verbose >=1 else range(self.n_iter)
+        range_ = tqdm(range(self.n_iter)) if verbose >= 1 else range(self.n_iter)
         for t in range_:
 
             q_distances = pairwise_distances(Y, metric=self.metric, squared=True)
@@ -59,8 +70,14 @@ class TSNE():
                 gradient = _compute_gradient(Y, self.early_exaggeration*P, Q, t_student_q_distances, self.n_components)
             else:
                 gradient = _compute_gradient(Y, P, Q, t_student_q_distances, self.n_components)
+
+            if t % self.interval_convergence_check == 0:
+                if _check_convergence(gradient, self.min_grad_norm):
+                    if verbose >= 1:
+                        print(f"Algorithm has converged at step {t}")
+                    return Y
                 
-            momentum = 0.5 if t < 250 else 0.8
+            momentum = self.momentum_rule(t)
             Y_t_plus_1 = Y - self.learning_rate * gradient + momentum * (Y - previous_Y)
 
             # update Ys
